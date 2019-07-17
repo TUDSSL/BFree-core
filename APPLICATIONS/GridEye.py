682d1c469a1234d5b3721f98ad76d3abc981b82a
Stefany Cruz <scruz010@ucr.edu>
2019-07-17 16:54:29 -0500

    Create GridEye.py

---
+++ APPLICATIONS/GridEye.py
@@ -0,0 +1,20 @@
+import time
+import busio
+import board
+import adafruit_amg88xx
+
+i2c = busio.I2C(board.SCL, board.SDA)
+amg = adafruit_amg88xx.AMG88XX(i2c)
+
+while True:
+    for row in amg.pixels:
+        # Pad to 1 decimal place
+        print(['{0:.1f}'.format(temp) for temp in row])
+        print("")
+
+    #print("\n")
+    for row in amg.pixels:
+        avg = sum(row)/len(row)
+        #print(avg)
+        print(round(avg,2))
+    time.sleep(1)
