7502be88a0e10169ebaf7792453708ca9515860e
Stefany Cruz <scruz010@ucr.edu>
2019-07-19 16:26:24 -0500

    Update Human_detection.py

--- APPLICATIONS/Human_detection.py
+++ APPLICATIONS/Human_detection.py
@@ -13,20 +13,16 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 
 while True:
     avg = 0
-    #avg_row.append([])
-    #avg_row.append([])
     for row in amg.pixels:
         # Pad to 1 decimal place
         print(['{0:.1f}'.format(temp) for temp in row])
         print("")
-
-    #print("\n")
     for row in range(len(amg.pixels)):
         for col in range(len(amg.pixels)):
             avg += amg.pixels[row][col]
     total = avg/64
     #print("total is ",total)
-    if total > 21.82:
+    if total > 21.82: # temp threshold for human detection
         print("human detected, average is ", total)
     else:
         print("no human detected, average is ", total)
