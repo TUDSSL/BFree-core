9558cf7e3a70cc1ba5e16b8314a7dc1b9a711056
Vito Kortbeek <vito.kortbeek@gmail.com>
2019-08-07 11:29:23 +0200

    Remove unused function

--- scripts/memdiff.py
+++ scripts/memdiff.py
@@ -2,20 +2,6 @@
 
 import os
 
-def group_consecutives(vals, step=1):
-    """Return list of consecutive lists of numbers from vals (number list)."""
-    run = []
-    result = [run]
-    expect = None
-    for v in vals:
-        if (v == expect) or (expect is None):
-            run.append(v)
-        else:
-            run = [v]
-            result.append(run)
-        expect = v + step
-    return result
-
 class MemDiff:
     def __init__(self, file_a, file_b, offset_addr=0):
         self.file_a = file_a
