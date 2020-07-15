93d939abedd9235f863d755124b95cf13f4512d3
Vito Kortbeek <vito.kortbeek@gmail.com>
2020-07-15 17:24:59 +0200

    [nvm-controller] Disable unused port settings

--- nvm-controller/src/ports.c
+++ nvm-controller/src/ports.c
@@ -5,41 +5,41 @@
 
 void portConfig(void)
 {
-    P1DIR = 0;                                              // Set P1 to input
-    P1REN = 0xFF;                                           // To enable pull up/down resistors for all 8 bits of P1
-    P1OUT = 0;                                              //  Pull down enable
+    //P1DIR = 0;                                              // Set P1 to input
+    //P1REN = 0xFF;                                           // To enable pull up/down resistors for all 8 bits of P1
+    //P1OUT = 0;                                              //  Pull down enable
 
-    P2OUT = 0;
-    P2DIR = 0xFF;
-    P2REN = 0;
+    //P2OUT = 0;
+    //P2DIR = 0xFF;
+    //P2REN = 0;
 
-    P3DIR = 0;
-    P3REN = 0xFF;
-    P3OUT = 0;
+    //P3DIR = 0;
+    //P3REN = 0xFF;
+    //P3OUT = 0;
 
-    P4DIR = 0;
-    P4REN = 0xFF;
-    P4OUT = 0;
+    //P4DIR = 0;
+    //P4REN = 0xFF;
+    //P4OUT = 0;
 
-    P5DIR = 0;
-    P5REN = 0xFF;
-    P5OUT = 0;
+    //P5DIR = 0;
+    //P5REN = 0xFF;
+    //P5OUT = 0;
 
-    P6DIR = 0;
-    P6REN = 0xFF;
-    P6OUT = 0;
+    //P6DIR = 0;
+    //P6REN = 0xFF;
+    //P6OUT = 0;
 
-    P7DIR = 0;
-    P7REN = 0xFF;
-    P7OUT = 0;
+    //P7DIR = 0;
+    //P7REN = 0xFF;
+    //P7OUT = 0;
 
-    P8DIR = 0;
-    P8REN = 0xFF;
-    P8OUT = 0;
+    //P8DIR = 0;
+    //P8REN = 0xFF;
+    //P8OUT = 0;
 
-    PJDIR = 0;
-    PJREN = 0xFF;
-    PJOUT = 0;
+    //PJDIR = 0;
+    //PJREN = 0xFF;
+    //PJOUT = 0;
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;                   // Clear the LOCKLPM5 bit in the PM5CTL0 register
